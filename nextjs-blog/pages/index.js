8e581c2198d349aaf8b28d38f08045f32dfd9dda
Hoàng Tấn <ithoangtan@gmail.com>
2020-06-01 23:59:28 +0700

    Pre-rendering Implement getStaticProps

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -35,3 +35,12 @@ export default function Home({ allPostsData }) {
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
